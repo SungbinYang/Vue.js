784b42bb18e4f5a792433a64ebda06627f452ce1
Robert.Yang <ysb05222@naver.com>
2021-06-12 04:03:32 +0900

    Computed - 캐싱
    Computed - 캐싱

--- vue-templete-syntax/src/App.vue
+++ vue-templete-syntax/src/App.vue
@@ -1,14 +1,26 @@
 <template>
-  <Fruits />
+  <h1>{{ reversedMessage }}</h1>
+  <h1>{{ reversedMessage }}</h1>
+  <h1>{{ reversedMessage }}</h1>
+  <h1>{{ reversedMessage }}</h1>
 </template>
 
 <script>
-
-import Fruits from '~/components/Fruits';
-
 export default {
-  components: {
-    Fruits
+  data() {
+    return {
+      msg: 'Hello Computed!'
+    }
+  },
+  computed: { // 캐싱 기능이 있어서 반복할 필요가 없음 성능 최적화
+    reversedMessage() {
+      return this.msg.split('').reverse().join('');
+    }
+  },
+  methods: { // 호출될때마다 불려져서 성능이 안 좋음
+    reverseMessage() {
+      return this.msg.split('').reverse().join('');
+    }
   }
 }
 </script>
