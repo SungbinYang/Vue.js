c7fc07ac35433b1bbb3f20dbb7195a21c2e45926
Robert.Yang <ysb05222@naver.com>
2021-06-12 04:14:57 +0900

    Getter, Setter
    Getter, Setter

--- vue-templete-syntax/src/App.vue
+++ vue-templete-syntax/src/App.vue
@@ -1,4 +1,7 @@
 <template>
+  <button @click="add">
+    Add
+  </button>
   <h1>{{ reversedMessage }}</h1>
   <h1>{{ reversedMessage }}</h1>
   <h1>{{ reversedMessage }}</h1>
@@ -9,17 +12,31 @@
 export default {
   data() {
     return {
+      // Getter, Setter 둘다 사용 가능
       msg: 'Hello Computed!'
     }
   },
   computed: { // 캐싱 기능이 있어서 반복할 필요가 없음 성능 최적화
-    reversedMessage() {
-      return this.msg.split('').reverse().join('');
+    // Getter로만 사용
+    // reversedMessage() {
+    //   return this.msg.split('').reverse().join('');
+    // }
+
+    // Getter, Setter로 재정의
+    reversedMessage: {
+      get() {
+        return this.msg.split('').reverse().join('');
+      },
+      set(value) {
+        // this.msg = value;
+        console.log(value);
+      }
     }
   },
   methods: { // 호출될때마다 불려져서 성능이 안 좋음
-    reverseMessage() {
-      return this.msg.split('').reverse().join('');
+    add() {
+      this.reversedMessage += '!?';
+      // this.msg += '!?';
     }
   }
 }
