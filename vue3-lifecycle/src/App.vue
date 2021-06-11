9dfea53d6cebf3e05cceb3b3b33dc4fe9bf87716
Robert.Yang <ysb05222@naver.com>
2021-06-12 01:40:39 +0900

    인스턴스와 라이프 사이클
    인스턴스와 라이프 사이클

---
+++ vue3-lifecycle/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1>{{ count }}</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 2
+    }
+  },
+  beforeCreate() {
+    console.log('Before Created', this.count); // undefined
+  },
+  created() {
+    console.log('Created!', this.count); // 2
+    console.log(document.querySelector('h1')); // null
+  },
+  beforeMount() {
+    console.log('Before Mounted');
+    console.log(document.querySelector('h1')); // null
+  },
+  mounted() {
+    console.log('Mounted');
+    console.log(document.querySelector('h1'));
+  }
+}
+</script>
