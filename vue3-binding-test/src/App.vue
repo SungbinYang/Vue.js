00741e3048f4003f3d9fc3e975ad3fd5718b4df7
Robert.Yang <ysb05222@naver.com>
2021-06-12 18:04:52 +0900

    v-model 수식어
    v-model 수식어

--- vue3-binding-test/src/App.vue
+++ vue3-binding-test/src/App.vue
@@ -2,20 +2,19 @@
   <h1>{{ msg }}</h1>
   <input
     type="text"
-    :value="msg"
-    @input="msg = $event.target.value" /> <!-- 한글 입력시 단방향 입력 바인딩으로 해야함 v-model을 쓸 시에 1박자 늦음 -->
-  <h1>{{ checked }}</h1>
-  <input
-    type="checkbox"
-    v-model="checked" />
+    v-model.trim="msg" />
 </template>
 
 <script>
 export default {
   data() {
     return {
-      msg: 'Hello World!',
-      checked: false
+      msg: 'HelloWorld'
+    }
+  },
+  watch: { // 변화된 내용 감지
+    msg() {
+      console.log(this.msg);
     }
   }
 }
