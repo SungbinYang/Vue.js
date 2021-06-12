898a1e3605b40b5d4f23bb834449b6002b1270c1
Robert.Yang <ysb05222@naver.com>
2021-06-12 12:16:54 +0900

    클래스와 스타일 바인딩
    클래스와 스타일 바인딩

--- vue-templete-syntax/src/App.vue
+++ vue-templete-syntax/src/App.vue
@@ -1,34 +1,28 @@
 <template>
-  <h1 @click="changeMessage">
-    {{ msg }}
+  <h1
+    :style="[fontStyle, backgroundStyle]"
+    @click="changeStyle">
+    Hello?!
   </h1>
-  <h1>{{ reversedMessage }}</h1>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      // Getter, Setter 둘다 사용 가능
-      msg: 'Hello?'
-    }
-  },
-  computed: {
-    reversedMessage() {
-      return this.msg.split('').reverse().join(''); // Getter
-    }
-  },
-  watch: { // 원본의 데이터뿐만 아니라 computed된 데이터도 감시를 할수 있다. 또한 this를 사용하지 않고 매개변수로 값을 받을수도 있다.
-    msg(newValue) {
-      console.log('msg: ', newValue); // Good!
-    },
-    reversedMessage(newValue) {
-      console.log('reversedMessage: ', newValue);
+      fontStyle: {
+        color: 'orange',
+        fontSize: '30px'
+      },
+      backgroundStyle: {
+        backgroundColor: 'black'
+      }
     }
   },
   methods: {
-    changeMessage() {
-      this.msg = 'Good!';
+    changeStyle() {
+      this.fontStyle.color = 'red';
+      this.fontStyle.fontSize = '50px';
     }
   }
 }
