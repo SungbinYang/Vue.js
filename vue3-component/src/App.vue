a7caa892a4c5a68f9aaea35b0a09fbbfcbe2368c
Robert.Yang <ysb05222@naver.com>
2021-06-12 21:25:50 +0900

    컴포넌트 - 기초
    컴포넌트 - 기초

---
+++ vue3-component/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <MyBtn>Banana</MyBtn>
+  <MyBtn :color="color">
+    <span style="color: red;">Apple</span>
+  </MyBtn>
+  <MyBtn
+    large
+    color="royalblue">
+    Cherry
+  </MyBtn>
+  <MyBtn>Orange</MyBtn>
+  <button>Banana</button>
+</template>
+
+<script>
+import MyBtn from '~/components/MyBtn'
+
+export default {
+  components: {
+    MyBtn
+  },
+  data() {
+    return {
+      color: '#000'
+    }
+  }
+}
+</script>
