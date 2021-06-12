a971ff00ae20a168b19bc3de3e4df94dfdaa404b
Robert.Yang <ysb05222@naver.com>
2021-06-12 15:25:06 +0900

    이벤트 핸들링
    이벤트 핸들링

---
+++ vue3-eventHandler-test/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <button @click="handlerA(), handlerB()">
+    Click me
+  </button>
+</template>
+
+<script>
+export default {
+  methods: {
+    handlerA() {
+      console.log('A');
+    },
+    handlerB() {
+      console.log('B');
+    }
+  }
+}
+</script>
